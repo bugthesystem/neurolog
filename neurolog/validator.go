5ee4af5a4278048327f82e3ca0a07473a1c9b414
Ziya SARIKAYA <ziya.sarikaya@here.com>
2016-12-20 14:49:24 +0100

    refactor validator

--- neurolog/validator.go
+++ neurolog/validator.go
@@ -6,36 +6,23 @@ import (
 )
 
 func _validateInput(network NeuralNetwork, input map[string]int64) {
-	keys := make([]interface{}, 0, len(input))
-	for k := range input {
-		keys = append(keys, k)
-	}
-
-	set1 := mapset.NewSetFromSlice(keys)
-	set2 := mapset.NewSet()
-	for k := range network._opts.Inputs {
-		set2.Add(k)
-	}
-
-	fmt.Println(set1)
-	fmt.Println(set2)
-
-	//TODO: fix set equality
-	//if !set1.Equal(set2) {
-	//	panic("Input does not have the required keys")
-	//}
+	_validate(network._opts.Inputs, input, "Input does not have the required keys")
 }
 
 func _validateOutput(network NeuralNetwork, output map[string]int64) {
-	keys := make([]interface{}, 0, len(output))
-	for k := range output {
+	_validate(network._opts.Outputs, output, "Output does not have the required keys")
+}
+
+func _validate(array []string, dataToValidate map[string]int64, message string) {
+	keys := make([]interface{}, 0, len(dataToValidate))
+	for k := range dataToValidate {
 		keys = append(keys, k)
 	}
 
 	set1 := mapset.NewSetFromSlice(keys)
 	set2 := mapset.NewSet()
 
-	for k := range network._opts.Outputs {
+	for k := range array {
 		set2.Add(k)
 	}
 
@@ -44,6 +31,6 @@ func _validateOutput(network NeuralNetwork, output map[string]int64) {
 
 	//TODO: fix set equality
 	//if !set1.Equal(set2) {
-	//	panic("Output does not have the required keys")
+	//	panic(message)
 	//}
 }
